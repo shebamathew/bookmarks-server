4e08b1aaa303515d247338f4cf927b9b7892168d
Sheba Mathew <sheba.m.mathew@gmail.com>
2019-03-05 20:19:15 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const winston = require('winston'); 
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+// data 
+const bookmarks = [{ 
+  id: 1,
+  title: 'Github',
+  url: 'http://www.github.com',
+  rating: '4',
+  desc: 'brings together the world\'s largest community of developers.'
+}]; 
+
+const lists = [{
+  id: 1, 
+  header: "Your Bookmarks", 
+  bookmarkIds: [1]
+}]
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(cors())
+app.use(helmet())
+
+app.use(function (req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  console.log(authToken);
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next){
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
+module.exports = app
