3dc3b85bb09e8d5bcf6cd19f115ca76bbaafcdca
Sheba Mathew <sheba.m.mathew@gmail.com>
2019-03-28 17:47:12 -0400

    adding delete request

--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -25,97 +25,80 @@ bookmarksRouter
       })
       .catch(next)
   })
-  .post (bodyParser, (req, res) => {
-    const { title, url, rating, desc } = req.body;
-  
-    if(!title) {
-      logger.error(`Title is required`);
-      return res
-        .status(400)
-        .send('Invalid data');
+  .post(bodyParser, (req, res, next) => {
+    for (const field of ['title', 'url', 'rating']) {
+      if (!req.body[field]) {
+        logger.error(`${field} is required`)
+        return res.status(400).send({
+          error: { message: `'${field}' is required` }
+        })
+      }
     }
   
-    if(!url) {
-      logger.error(`URL is required`);
-      return res
-        .status(400)
-        .send('Invalid data');
+    const { title, url, description, rating } = req.body
+
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`Invalid rating '${rating}' supplied`)
+      return res.status(400).send({
+        error: { message: `'rating' must be a number between 0 and 5` }
+      })
     }
-  
-    if(!rating) {
-      logger.error(`Rating is required`);
-      return res
-        .status(400)
-        .send('Invalid data');
+
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`)
+      return res.status(400).send({
+        error: { message: `'url' must be a valid URL` }
+      })
     }
-  
-    if(!desc) {
-      logger.error(`Description is required`);
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-  
-    const id = uuid();
-  
-    const bookmark = {
-      id,
-      title,
-      url, 
-      rating, 
-      desc
-    };
-  
-    bookmarks.push(bookmark);
-  
-    logger.info(`Bookmark with id ${id} created`);
-    
-    res
-      .status(201)
-      .location(`http://localhost:8000/bookmark/${id}`)
-      .json({bookmark});
+
+    const newBookmark = { title, url, description, rating }
+
+    BookarksService.insertBookmark(
+      req.app.get('db'),
+      newBookmark
+    )
+      .then(bookmark => {
+        logger.info(`Bookmark with id ${bookmark.id} created.`)
+        res
+          .status(201)
+          .location(`/bookmarks/${bookmark.id}`)
+          .json(serializeBookmark(bookmark))
+      })
+      .catch(next)
   })
 
-bookmarksRouter
-  .route('/bookmark/:id')
+
+  bookmarksRouter
+  .route('/bookmarks/:bookmark_id')
+  .all((req, res, next) => {
+    const { bookmark_id } = req.params
+    BookarksService.getById(req.app.get('db'), bookmark_id)
+      .then(bookmark => {
+        if (!bookmark) {
+          logger.error(`Bookmark with id ${bookmark_id} not found.`)
+          return res.status(404).json({
+            error: { message: `Bookmark Not Found` }
+          })
+        }
+        res.bookmark = bookmark
+        next()
+      })
+      .catch(next)
+  })
   .get((req, res) => {
-    const { id } = req.params;
-    const bookmark = bookmarks.find(b => b.id == id);
-  
-    if (!bookmark) {
-      logger.error(`Bookmark with id ${id} not found.`);
-      return res
-        .status(404)
-        .send('Bookmark Not Found');
-    }
-    res.json(bookmark);
+    res.json(serializeBookmark(res.bookmark))
   })
-  .delete((req, res) => {
-    const { id } = req.params;
+  .delete((req, res, next) => {
+    const { bookmark_id } = req.params
+    BookarksService.deleteBookmark(
+      req.app.get('db'),
+      bookmark_id
+    )
+      .then(numRowsAffected => {
+        logger.info(`Bookmark with id ${bookmark_id} deleted.`)
+        res.status(204).end()
+      })
+      .catch(next)
+  })
 
-    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
-  
-    if (bookmarkIndex === -1) {
-      logger.error(`bookmark with id ${id} not found.`);
-      return res
-        .status(404)
-        .send('Not found');
-    }
-  
-    //remove bookmark from lists
-    //assume bookmarkIds are not duplicated in the bookmarkIds array
-    lists.forEach(list => {
-      const bookmarkIds = list.bookmarkIds.filter(bId => bId !== id);
-      list.bookmarkIds = bookmarkIds;
-    });
-  
-    bookmarks.splice(bookmarkIndex, 1);
-  
-    logger.info(`bookmark with id ${id} deleted.`);
-  
-    res
-      .status(204)
-      .end();
-    })
-
-module.exports = bookmarkRouter
+module.exports = bookmarksRouter
